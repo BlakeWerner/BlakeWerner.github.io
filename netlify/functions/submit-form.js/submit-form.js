29b980b44a3339a952feb3a8422b370b94be138f
BlakeWerner <128490652+BlakeWerner@users.noreply.github.com>
2025-06-07 12:26:15 -0500

    Delete submit-form.js

--- netlify/functions/submit-form.js/submit-form.js
+++
@@ -1,8 +0,0 @@
-// netlify/functions/submit-form.js
-exports.handler = async (event) => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Function is working!' }),
-    };
-  };
-  
